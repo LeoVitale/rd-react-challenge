e35e763f3f86b052f8bf3f0a2971da51ce2cb9f7
Leonardo Vitale <leonardo_vitale@hotmail.com>
2016-12-21 21:07:01 -0200

    Installed react-md

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import './App.css'
+import './sass/index.scss';
 import React, {Component} from 'react';
+import WebFontLoader from 'webfontloader';
+
+import { Card, CardTitle, CardText, CardActions } from 'react-md/lib/Cards';
+import Button from 'react-md/lib/Buttons';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
 
 class App extends Component {
   render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
+    return (
+      <div className="md-grid">
+        <Card className="md-cell">
+          <CardTitle title="Hello, World!"  />
+          <CardText>
+            Lorem ipsum... pretend more ...
+          </CardText>
+          <CardActions>
+            <Button flat label="Action 1" />
+            <Button flat label="Action 2" />
+          </CardActions>
+        </Card>
       </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and safggvssess to hot reload your changes.</p>
-      </div>
-    </div>
+    )
   }
 }
 
